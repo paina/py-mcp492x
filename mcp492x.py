db6ed5e450bcb4472f1990cc59a2789b1710a0ab
SATO Taisuke <paina@paina-m01.local>
2019-09-18 14:54:27 +0900

    Fix errors.

--- mcp492x.py
+++ mcp492x.py
@@ -11,16 +11,16 @@ class Mcp492xDac:
         self.chip = chip
         self.ab = ab
 
-    def write(data, buf=1, ga_=1, shdn_=1):
+    def write(self, data, buf=1, ga_=1, shdn_=1):
         ab = self.ab
 
         cmd = int(0)
-        cmd += ab    << __BIT["AB"]
-        cmd += buf   << __BIT["BUF"]
-        cmd += ga_   << __BIT["GA_"]
-        cmd += shdn_ << __BIT["SHDN_"]
+        cmd += ab    << self.__BIT["AB"]
+        cmd += buf   << self.__BIT["BUF"]
+        cmd += ga_   << self.__BIT["GA_"]
+        cmd += shdn_ << self.__BIT["SHDN_"]
 
-        cmd += data  << (__DATA_BITS - 1)
+        cmd += data  << (self.__DATA_BITS - 1)
 
         cmd_bytes = cmd.to_bytes
         return self.chip.__xfer(cmd_bytes)
@@ -35,7 +35,7 @@ class Mcp4921(Mcp492x):
     def __init__(self, bus):
         super.__init__
 
-    def __xfer(data):
+    def __xfer(self, data):
         raise "not implemented"
 
 # MCP4921 is SPI version of MCP492x
@@ -43,5 +43,5 @@ class Mcp4922(Mcp492x):
     def __init__(self, bus):
         super.__init__
         
-    def __xfer(data):
+    def __xfer(self, data):
         self.bus.xfer(data)
